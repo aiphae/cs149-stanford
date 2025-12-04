f3a2d774f51ae77773b38055411e8c9ed7220143
aiphae <vl.diachenkoo@gmail.com>
2025-12-04 20:28:44 +0000

    Complete Part 1

--- asst3/saxpy/saxpy.cu
+++ asst3/saxpy/saxpy.cu
@@ -1,45 +1,36 @@
 #include <stdio.h>
-
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <driver_functions.h>
-
 #include "CycleTimer.h"
 
-
-// return GB/sec
+// Return GB/sec
 float GBPerSec(int bytes, float sec) {
   return static_cast<float>(bytes) / (1024. * 1024. * 1024.) / sec;
 }
 
-
-// This is the CUDA "kernel" function that is run on the GPU.  You
+// This is the CUDA "kernel" function that is run on the GPU. You
 // know this because it is marked as a __global__ function.
-__global__ void
-saxpy_kernel(int N, float alpha, float* x, float* y, float* result) {
-
+__global__
+void saxpy_kernel(int N, float alpha, float* x, float* y, float* result) {
     // compute overall thread index from position of thread in current
     // block, and given the block we are in (in this example only a 1D
     // calculation is needed so the code only looks at the .x terms of
     // blockDim and threadIdx.
     int index = blockIdx.x * blockDim.x + threadIdx.x;
 
-
     // this check is necessary to make the code work for values of N
     // that are not a multiple of the thread block size (blockDim.x)
-    if (index < N)
-       result[index] = alpha * x[index] + y[index];
+    if (index < N) {
+        result[index] = alpha * x[index] + y[index];
+    }
 }
 
-
-// saxpyCuda --
-//
 // This function is regular C code running on the CPU.  It allocates
 // memory on the GPU using CUDA API functions, uses CUDA API functions
 // to transfer data from the CPU's memory address space to GPU memory
 // address space, and launches the CUDA kernel function on the GPU.
 void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) {
-
     // must read both input arrays (xarray and yarray) and write to
     // output array (resultarray)
     int totalBytes = sizeof(float) * 3 * N;
@@ -65,33 +56,23 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     float* device_x = nullptr;
     float* device_y = nullptr;
     float* device_result = nullptr;
-    
-    //
-    // CS149 TODO: allocate device memory buffers on the GPU using cudaMalloc.
-    //
-    // We highly recommend taking a look at NVIDIA's
-    // tutorial, which clearly walks you through the few lines of code
-    // you need to write for this part of the assignment:
-    //
-    // https://devblogs.nvidia.com/easy-introduction-cuda-c-and-c/
-    //
+
+    cudaMalloc(&device_x, sizeof(float) * N);
+    cudaMalloc(&device_y, sizeof(float) * N);
+    cudaMalloc(&device_result, sizeof(float) * N);
         
     // start timing after allocation of device memory
     double startTime = CycleTimer::currentSeconds();
 
-    //
-    // CS149 TODO: copy input arrays to the GPU using cudaMemcpy
-    //
-
+    cudaMemcpy(device_x, xarray, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_y, yarray, sizeof(float) * N, cudaMemcpyHostToDevice);
    
     // run CUDA kernel. (notice the <<< >>> brackets indicating a CUDA
     // kernel launch) Execution on the GPU occurs here.
     saxpy_kernel<<<blocks, threadsPerBlock>>>(N, alpha, device_x, device_y, device_result);
 
-    //
-    // CS149 TODO: copy result from GPU back to CPU using cudaMemcpy
-    //
-
+    cudaDeviceSynchronize();
+    cudaMemcpy(resultarray, device_result, sizeof(float) * N, cudaMemcpyDeviceToHost);
     
     // end timing after result has been copied back into host memory
     double endTime = CycleTimer::currentSeconds();
@@ -105,10 +86,9 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     double overallDuration = endTime - startTime;
     printf("Effective BW by CUDA saxpy: %.3f ms\t\t[%.3f GB/s]\n", 1000.f * overallDuration, GBPerSec(totalBytes, overallDuration));
 
-    //
-    // CS149 TODO: free memory buffers on the GPU using cudaFree
-    //
-    
+    cudaFree(device_x);
+    cudaFree(device_y);
+    cudaFree(device_result);
 }
 
 void printCudaInfo() {
